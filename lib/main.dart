39c55c95ad04a81d87c4be06a47e05fe714cfe4d
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-27 13:32:45 +0800

    Learn Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600]
       ),
       body: Center(
-        child: Text(
-          'Hello World',
-            style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-        )
-        )
+        child: Image.asset('assets/images/1.jpg')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
