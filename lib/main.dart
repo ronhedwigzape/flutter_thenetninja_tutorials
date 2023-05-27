35ab08dfdcb45d83ccabe974715f34da8fb1f9a9
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-27 12:27:32 +0800

    Learn Stateless Widgets & Hot-Reload

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  /* Scaffold()
-   * A Scaffold is like wrapper to
-   * different layouts like appbar,
-   * body, floatingActionButton etc.
-   */
-    home: Scaffold(
+    home: Home()
+  ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  /*  build()
+      This method is called whenever
+      the state of the app changes.
+      It is called again and again.
+  */
+  Widget build(BuildContext context) {
+  /*  Scaffold()
+      A Scaffold is like wrapper to
+      different layouts like appbar,
+      body, floatingActionButton etc.
+  */
+    return Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
         centerTitle: true,
-        backgroundColor: Colors.red[600]  ,
+        backgroundColor: Colors.red[600]
       ),
       body: Center(
         child: Text(
@@ -22,15 +35,15 @@ void main() => runApp(MaterialApp(
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-        ),
-        ),
+            fontFamily: 'IndieFlower'
+        )
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
-        child: Text('Click'),
+        child: Text('Click')
       )
-    ),
-  ));
-
+    );
+  }
+}
